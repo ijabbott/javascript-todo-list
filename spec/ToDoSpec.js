8e96b17dfb44afe8630b2b8c61344038873edd07
Ian Abbott <ij.abbott2306@gmail.com>
2019-03-26 11:48:41 -0500

    Adding delete behavior with passing test

--- spec/ToDoSpec.js
+++ spec/ToDoSpec.js
@@ -1,13 +1,28 @@
-describe('ToDo', ()=>{
+describe('ToDo', () => {
 	describe('Testing the functionality, this is the checklist', ()=>{
-		it('should add an item', ()=>{
+		it('should add an item', () => {
 			let todo = new ToDo();
 			let item = {
 				title: "get milk",
 				complete: false
-			}
-			const done = todo.addTodo(item)
+			};
+
+			todo.addTodo(item);
+
 			expect(todo.getItems().length).toBe(1);
-		})
-	})
-})
+		});
+
+		it('should delete an existing item', () => {
+			let todo = new ToDo();
+			let item = {
+				title: "get milk",
+				complete: false
+			};
+
+			todo.addTodo(item);
+			todo.deleteTodo();
+
+			expect(todo.getItems().length).toBe(0);
+		});
+	});
+});
--- src/ToDo.js
+++ src/ToDo.js
@@ -1,11 +1,15 @@
-function ToDo(){
+function ToDo() {
 	this.todo = [];
 }
 
-ToDo.prototype.addTodo= function(item){
+ToDo.prototype.addTodo = function(item) {
 	this.todo.push(item)
-}
+};
 
-ToDo.prototype.getItems= function(){
+ToDo.prototype.getItems = function() {
 	return this.todo
-}
+};
+
+ToDo.prototype.deleteTodo = function() {
+	this.todo.pop()
+};
